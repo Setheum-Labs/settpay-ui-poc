0d5887ec36216ea7b820a7b3d69e014e3eb0dc43
Muhammad-Jibril <jbashir52@gmail.com>
2021-05-13 04:41:32 +0800

    Initialize file structure

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/iamkhalifaMBA/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/iamkhalifaMBA/Documents/GitHub/settpay-ui-poc/settpay"
-export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_APPLICATION_PATH=/home/iamkhalifaMBA/Documents/GitHub/settpay-ui-poc"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
